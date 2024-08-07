4ff96d1c9ee51c6d121324a4ca8efdc5e2ced0c2
kwrona05 <kwrona05@gmail.com>
2024-08-07 10:57:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const textInput = document.getElementById("text-input")
-const checkBtn = document.getElementById("check-btn")
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const resultText = document.getElementById("result");
+
+function palindromeCheck() {
+    const textInput = textInput.value
+    if (textInput === "") {
+        resultText.textContent = "Please input a value"
+        resultText = resultText.textContent
+    } else if (textInput === "A") {
+        resultText.textContent = "A is palindrome"
+        resultText = resultText.textContent
+
+    }
+}
+
+checkBtn.addEventListener('click', palindromeCheck)
